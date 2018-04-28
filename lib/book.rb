2df7960efd4845edb6f78214fe7b3e6b55868ffd
Ian G Canino <ian.canino@gmail.com>
2018-04-28 01:24:34 -0400

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,24 @@
 
+class Book
+  attr_accessor :title, :author
+  attr_reader :page_count, :genre
 
+  def initialize(title)
+    @title = title
+    @author
+    @page_count
+  end
+
+  def page_count=(num)
+    @page_count = num
+  end
+
+  def genre=(category)
+    @genre = category
+  end
+
+  def turn_page
+    puts 'Flipping the page...wow, you read fast!'
+
+  end
+end
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,10 @@
-# Make your shoe class here!
+class Shoe
+  attr_accessor :color, :size, :naterial
+  attr_reader :brand
+
+  def initialize(brand_name)
+    @brand = brand_name
+  end
+
+end
+
